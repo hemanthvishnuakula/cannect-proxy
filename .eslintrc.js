dad8b54485b2a2eee5ca67c42028c98594cc02da
Hemanth Vishnu <hello@hemanthvishnu.com>
2025-12-29 06:59:12 +0000

    style: format eslintrc with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,22 +8,25 @@ module.exports = {
   rules: {
     // Prettier integration
     'prettier/prettier': 'warn',
-    
+
     // React Native specific
     'react-native/no-unused-styles': 'warn',
     'react-native/no-inline-styles': 'off', // Allow inline for NativeWind
-    
+
     // React
     'react/react-in-jsx-scope': 'off', // Not needed in React 17+
     'react-hooks/exhaustive-deps': 'warn',
-    
+
     // TypeScript
-    '@typescript-eslint/no-unused-vars': ['warn', { 
-      argsIgnorePattern: '^_',
-      varsIgnorePattern: '^_',
-    }],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
     '@typescript-eslint/no-explicit-any': 'off', // Too strict for now
-    
+
     // General
     'no-console': ['warn', { allow: ['warn', 'error'] }],
   },
